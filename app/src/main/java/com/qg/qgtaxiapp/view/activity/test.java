59c100dbf7d0ac574d9620b78c080446bff02174
Hx <1349919459@qq.com>
2021-08-09 22:23:19 +0800

    尝试测试折线图

--- app/src/main/java/com/qg/qgtaxiapp/view/activity/test.java
+++ app/src/main/java/com/qg/qgtaxiapp/view/activity/test.java
@@ -8,33 +8,18 @@ import android.view.LayoutInflater;
 import com.qg.qgtaxiapp.R;
 import com.qg.qgtaxiapp.databinding.ActivityTestBinding;
 
-import com.qg.qgtaxiapp.myview.MyView;
-
 public class test extends AppCompatActivity {
 
 
     private ActivityTestBinding binding;
-    private MyView myView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         binding = ActivityTestBinding.inflate(LayoutInflater.from(this));
 
-        myView = binding.linechart;
 
-        new Thread(){
-            public void run(){
-                while (true){
-                    try {
-                        Thread.sleep(1000);
-                        myView.setNewData((float)Math.random(),(float)Math.random());
-                    }catch (InterruptedException e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }.start();
 
     }
 }
