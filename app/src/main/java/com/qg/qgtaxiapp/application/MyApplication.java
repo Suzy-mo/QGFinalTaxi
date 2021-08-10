6318ce4fd803592bcb65b66e68c51af403487a07
TJHfengche <762795632@qq.com>
2021-08-10 22:32:21 +0800

    重新分包

---
+++ app/src/main/java/com/qg/qgtaxiapp/application/MyApplication.java
@@ -0,0 +1,7 @@
+package com.qg.qgtaxiapp.application;/**
+ * Created with Android studio
+ * @Author: EDGClearlove7
+ * @Date: 2021/08/10/20:26
+ * @Description: 
+ */public class MyApplication {
+}
