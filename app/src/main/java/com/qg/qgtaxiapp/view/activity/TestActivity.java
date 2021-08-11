cd5dd4c1955de5a0b18e35a2ad4885f1f25c96d4
Hx <1349919459@qq.com>
2021-08-11 10:53:29 +0800

    删去定位功能

--- app/src/main/java/com/qg/qgtaxiapp/view/activity/TestActivity.java
+++ app/src/main/java/com/qg/qgtaxiapp/view/activity/TestActivity.java
@@ -1,12 +1,8 @@
 package com.qg.qgtaxiapp.view.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.Manifest;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -14,42 +10,29 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.amap.api.location.AMapLocation;
-import com.amap.api.location.AMapLocationClient;
-import com.amap.api.location.AMapLocationClientOption;
-import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.AMapOptions;
-import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
-import com.amap.api.maps.model.HeatmapTileProvider;
 import com.amap.api.maps.model.LatLng;
-import com.amap.api.maps.model.TileOverlayOptions;
-import pub.devrel.easypermissions.AfterPermissionGranted;
-import pub.devrel.easypermissions.EasyPermissions;
 import com.qg.qgtaxiapp.databinding.ActivityTestBinding;
 import com.qg.qgtaxiapp.utils.MapUtils;
 
-import static com.amap.api.maps.model.HeatmapTileProvider.DEFAULT_GRADIENT;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
-public class TestActivity extends AppCompatActivity implements AMapLocationListener, LocationSource {
+public class TestActivity extends AppCompatActivity{
 
     //请求权限码
     private static final int REQUEST_PERMISSIONS = 9527;
-    //声明AMapLocationClient类对象
-    public AMapLocationClient mLocationClient = null;
-    //声明AMapLocationClientOption对象
-    public AMapLocationClientOption mLocationOption = null;
+
     private ActivityTestBinding binding;
     private UiSettings uiSettings;
-    //内容
-    //private TextView tvContent;
+
     private MapView mapView;
     //地图控制器
     private AMap aMap = null;
-    //位置更改监听
-    private OnLocationChangedListener mListener;
 
     private MapUtils mapUtils = new MapUtils();
 
@@ -62,55 +45,14 @@ public class TestActivity extends AppCompatActivity implements AMapLocationListe
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         mapView = binding.mapView;
         mapView.onCreate(savedInstanceState);
-        //初始化定位
-        initLocation();
+
         //初始化地图
         initMap(savedInstanceState);
-
         //检查安卓版本
         deleteLogo();
         checkingAndroidVersion();
 
     }
-
-    /**
-     * 接收异步返回的定位结果
-     *
-     * @param aMapLocation
-     */
-    @Override
-    public void onLocationChanged(AMapLocation aMapLocation) {
-        if (aMapLocation != null) {
-            if (aMapLocation.getErrorCode() == 0) {
-                //地址
-                String address = aMapLocation.getAddress();
-                //tvContent.setText(address == null ? "无地址" : address);
-
-                Log.d("TAG",aMapLocation.getLatitude() + "");
-                Log.d("TAG",aMapLocation.getLongitude() + "");
-                Log.d("MAinActivity",address);
-                showMsg(address);
-                mLocationClient.stopLocation();
-
-                LatLng[] latlngs = mapUtils.initHeatMapData(aMapLocation.getLatitude(),aMapLocation.getLongitude());
-
-                HeatmapTileProvider heatmapTileProvider = mapUtils.initBuildHeatmapTileProvider(latlngs);
-                TileOverlayOptions tileOverlayOptions = new TileOverlayOptions();
-                tileOverlayOptions.tileProvider(heatmapTileProvider);
-                aMap.addTileOverlay(tileOverlayOptions);
-
-                if(mListener != null){
-                    mListener.onLocationChanged(aMapLocation);
-                }
-            } else {
-                //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
-                Log.e("AmapError", "location Error, ErrCode:"
-                        + aMapLocation.getErrorCode() + ", errInfo:"
-                        + aMapLocation.getErrorInfo());
-            }
-        }
-    }
-
 
     /**
      * 检查Android版本
@@ -120,9 +62,6 @@ public class TestActivity extends AppCompatActivity implements AMapLocationListe
             //Android6.0及以上先获取权限再定位
             requestPermission();
 
-        } else {
-            //Android6.0以下直接定位
-            mLocationClient.startLocation();
         }
     }
 
@@ -139,11 +78,8 @@ public class TestActivity extends AppCompatActivity implements AMapLocationListe
         };
 
         if (EasyPermissions.hasPermissions(this, permissions)) {
-            //true 有权限 开始定位
-            showMsg("已获得权限，可以定位啦！");
+            showMsg("权限已获取");
 
-            //启动定位
-            mLocationClient.startLocation();
         } else {
             //false 无权限
             EasyPermissions.requestPermissions(this, "需要权限", REQUEST_PERMISSIONS, permissions);
@@ -171,36 +107,11 @@ public class TestActivity extends AppCompatActivity implements AMapLocationListe
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
-    /**
-     * 初始化定位
-     */
-    private void initLocation() {
 
-        //初始化定位
-        mLocationClient = new AMapLocationClient(getApplicationContext());
-        //设置定位回调监听
-        mLocationClient.setLocationListener(this);
-        //初始化AMapLocationClientOption对象
-        mLocationOption = new AMapLocationClientOption();
-        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
-        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-        //获取最近3s内精度最高的一次定位结果：
-        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
-        mLocationOption.setOnceLocationLatest(true);
-        //设置是否返回地址信息（默认返回地址信息）
-        mLocationOption.setNeedAddress(true);
-        //设置定位请求超时时间，单位是毫秒，默认30000毫秒，建议超时时间不要低于8000毫秒。
-        mLocationOption.setHttpTimeOut(20000);
-        //关闭缓存机制，高精度定位会产生缓存。
-        mLocationOption.setLocationCacheEnable(false);
-        //给定位客户端对象设置定位参数
-        mLocationClient.setLocationOption(mLocationOption);
-    }
 
     @Override
     protected void onDestroy() {
         //销毁定位客户端，同时销毁本地定位服务。
-        mLocationClient.onDestroy();
         mapView.onDestroy();
         super.onDestroy();
     }
@@ -241,36 +152,11 @@ public class TestActivity extends AppCompatActivity implements AMapLocationListe
                         .setEnable(true)
                         .setStyleId("7f431e5f5cf8c616d10cfaa2907a229e")//官网控制台-自定义样式 获取
         );
-        // 设置定位监听
-        aMap.setLocationSource(this);
-        // 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
-        aMap.setMyLocationEnabled(true);
-        aMap.showIndoorMap(true);
+
+        LatLng latLng = new LatLng(23.129112,113.264385);//广州市
+        aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,11));
     }
 
-    /**
-     * 激活定位
-     */
-    @Override
-    public void activate(OnLocationChangedListener onLocationChangedListener) {
-        mListener = onLocationChangedListener;
-        if (mLocationClient == null) {
-            mLocationClient.startLocation();//启动定位
-        }
-    }
-
-    /**
-     * 停止定位
-     */
-    @Override
-    public void deactivate() {
-        mListener = null;
-        if (mLocationClient != null) {
-            mLocationClient.stopLocation();
-            mLocationClient.onDestroy();
-        }
-        mLocationClient = null;
-    }
 
     private void deleteLogo(){
         UiSettings settings=aMap.getUiSettings();
