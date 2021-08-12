57f38a2894790c71b5d779bc312666e94e728e13
Hx <1349919459@qq.com>
2021-08-12 16:58:31 +0800

    完成热力图模块的界面的初步设置和初步逻辑编写

--- app/src/main/java/com/qg/qgtaxiapp/view/activity/MainActivity.java
+++ app/src/main/java/com/qg/qgtaxiapp/view/activity/MainActivity.java
@@ -1,10 +1,15 @@
 package com.qg.qgtaxiapp.view.activity;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -17,6 +22,9 @@ import com.qg.qgtaxiapp.databinding.ActivityMainBinding;
 
 import java.util.ArrayList;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
 /**
  * Created with Android studio
  *
@@ -25,8 +33,16 @@ import java.util.ArrayList;
  * @Description:
  */
 public class MainActivity extends AppCompatActivity {
+
+    /*
+        保存MyTouchListener接口的列表
+    */
+    private ArrayList<MyTouchListener> myTouchListeners = new ArrayList<>();
+
     private ArrayList<Fragment> fragments = new ArrayList<>();
     private ActivityMainBinding binding;
+    //请求权限码
+    private static final int REQUEST_PERMISSIONS = 9527;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,5 +54,85 @@ public class MainActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    /**
+     * 检查Android版本
+     */
+    private void checkingAndroidVersion() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //Android6.0及以上先获取权限再定位
+            requestPermission();
+
+        }
+    }
+
+    /**
+     * 动态请求权限
+     */
+    @AfterPermissionGranted(REQUEST_PERMISSIONS)
+    private void requestPermission() {
+        String[] permissions = {
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        if (EasyPermissions.hasPermissions(this, permissions)) {
+            showMsg("权限已获取");
+
+        } else {
+            //false 无权限
+            EasyPermissions.requestPermissions(this, "需要权限", REQUEST_PERMISSIONS, permissions);
+        }
+    }
+
+
+    /**
+     * 请求权限结果
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //设置权限请求结果
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    /**
+     * Toast提示
+     * @param msg 提示内容
+     */
+    private void showMsg(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public interface MyTouchListener {
+        /** onTOuchEvent的实现 */
+        boolean onTouchEvent(MotionEvent event);
+    }
+    /**
+     * 提供给Fragment通过getActivity()方法来注册自己的触摸事件的方法
+     */
+    public void registerMyTouchListener(MyTouchListener listener) {
+        myTouchListeners.add(listener);
+    }
+
+    /**
+     * 提供给Fragment通过getActivity()方法来取消注册自己的触摸事件的方法
+     */
+    public void unRegisterMyTouchListener(MyTouchListener listener) {
+        myTouchListeners.remove( listener );
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        for (MyTouchListener listener : myTouchListeners){
+            listener.onTouchEvent(ev);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
