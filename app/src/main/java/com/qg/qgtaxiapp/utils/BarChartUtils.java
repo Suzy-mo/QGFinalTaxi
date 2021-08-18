362648648ca41f18ffb8e96c37c19d7787f54a12
Hx <1349919459@qq.com>
2021-08-18 11:57:32 +0800

    修改了柱状图一些样式

--- app/src/main/java/com/qg/qgtaxiapp/utils/BarChartUtils.java
+++ app/src/main/java/com/qg/qgtaxiapp/utils/BarChartUtils.java
@@ -1,7 +1,11 @@
 package com.qg.qgtaxiapp.utils;
 
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
 
+import com.github.mikephil.charting.animation.ChartAnimator;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.Legend;
@@ -14,6 +18,8 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IValueFormatter;
 import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+import com.github.mikephil.charting.renderer.BarChartRenderer;
 import com.github.mikephil.charting.utils.ViewPortHandler;
 
 import java.util.ArrayList;
@@ -30,6 +36,7 @@ public class BarChartUtils {
     private BarChart bar;
     List<BarEntry> list = new ArrayList<>();
 
+
     public BarChartUtils(BarChart bar, List<BarEntry> list) {
         this.bar = bar;
         this.list = list;
@@ -40,11 +47,17 @@ public class BarChartUtils {
         BarData barData=new BarData(barDataSet);
         bar.setData(barData);
 
+        barData.setBarWidth(0.5f);
+
+        //柱状图手势交互
+        bar.setTouchEnabled(false);//是否有手势操作
+        bar.setScaleEnabled(false);//是否可缩放
 
         //折线图背景
-        bar.setBackgroundColor(Color.parseColor("#FF1F1F1F"));   //背景颜色
+        //bar.setBackgroundColor(Color.parseColor("#FF1F1F1F"));   //背景颜色
         bar.getXAxis().setDrawGridLines(true);  //是否绘制X轴上的网格线（背景里面的竖线）
         bar.getAxisLeft().setDrawGridLines(true);  //是否绘制Y轴上的网格线（背景里面的横线）
+        bar.setFitBars(true);
 
         //对于右下角一串字母的操作
         bar.getDescription().setEnabled(false);                  //是否显示右下角描述
@@ -53,13 +66,13 @@ public class BarChartUtils {
         bar.getDescription().setTextColor(Color.RED);             //字体颜色
 
 //        //图例
-//        Legend legend=bar.getLegend();
-//        legend.setEnabled(true);    //是否显示图例
+       Legend legend = bar.getLegend();
+       legend.setEnabled(false);    //是否显示图例
 //        legend.setPosition(Legend.LegendPosition.BELOW_CHART_CENTER);    //图例的位置
 
         //X轴
         XAxis xAxis=bar.getXAxis();
-        xAxis.setDrawGridLines(true);  //是否绘制X轴上的网格线（背景里面的竖线）
+        xAxis.setDrawGridLines(false);  //是否绘制X轴上的网格线（背景里面的竖线）
         xAxis.setGridColor(Color.parseColor("#61FFFFFF"));
         xAxis.setAxisLineColor(Color.parseColor("#61FFFFFF"));   //X轴颜色
         xAxis.setAxisLineWidth(3);           //X轴粗细
@@ -74,7 +87,7 @@ public class BarChartUtils {
                     return "早上";
                 }
                 if (value==3){
-                    return "中午";
+                    return "下午";
                 }
                 if (value==4){
                     return "晚上";
@@ -90,9 +103,11 @@ public class BarChartUtils {
 
         //Y轴
         YAxis AxisLeft=bar.getAxisLeft();
-        AxisLeft.setDrawGridLines(false);  //是否绘制Y轴上的网格线（背景里面的横线）
-        AxisLeft.setAxisLineColor(Color.BLUE);  //Y轴颜色
-        AxisLeft.setAxisLineWidth(2);           //Y轴粗细
+        AxisLeft.setDrawGridLines(true);  //是否绘制Y轴上的网格线（背景里面的横线）
+        AxisLeft.setAxisLineColor(Color.argb(1,41,41,41));  //Y轴颜色
+        AxisLeft.setGridColor(Color.parseColor("#61FFFFFF"));
+        AxisLeft.setGridLineWidth(1);
+        AxisLeft.setAxisLineWidth(3);           //Y轴粗细
         AxisLeft.setValueFormatter(new ValueFormatter() {
             @Override
             public String getAxisLabel(float value, AxisBase axis) {
@@ -104,17 +119,19 @@ public class BarChartUtils {
                 return "";
             }
         });
-        AxisLeft.setAxisMaximum(130);   //Y轴最大数值
-        AxisLeft.setAxisMinimum(0);   //Y轴最小数值
+        AxisLeft.setAxisMaximum(130f);   //Y轴最大数值
+        AxisLeft.setAxisMinimum(0f);   //Y轴最小数值
+        AxisLeft.setGranularity(20f); //Y轴标签间隔
         //Y轴坐标的个数    第二个参数一般填false     true表示强制设置标签数 可能会导致X轴坐标显示不全等问题
         AxisLeft.setLabelCount(100,false);
 
         //是否隐藏右边的Y轴（不设置的话有两条Y轴 同理可以隐藏左边的Y轴）
         bar.getAxisRight().setEnabled(false);
-        bar.getAxisLeft().setEnabled(false);
+        bar.getAxisLeft().setEnabled(true);
 
 
         //柱子
+        barDataSet.setDrawValues(false);//是否显示柱子上的文字
         barDataSet.setColor(Color.parseColor("#FF03DAC5"));  //柱子的颜色
         //barDataSet.setColors(Color.BLACK,Color.BLUE);//设置柱子多种颜色  循环使用
         barDataSet.setBarBorderColor(Color.parseColor("#FF03DAC5"));//柱子边框颜色
@@ -142,7 +159,7 @@ public class BarChartUtils {
         bar.invalidate();
 
         //动画（如果使用了动画可以则省去更新数据的那一步）
-        bar.animateY(3000); //在Y轴的动画  参数是动画执行时间 毫秒为单位
+        bar.animateY(500); //在Y轴的动画  参数是动画执行时间 毫秒为单位
     }
     public void setBar(){
         BarDataSet barDataSet=new BarDataSet(list,"司机收入分析");   //list是你这条线的数据  "语文" 是你对这条线的描述
